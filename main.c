bfc11a99bfdab9797fe4e49b092b99b10e86cb81
changhan <changhanlee@gmail.com>
2016-03-09 13:35:12 -0500

    OH노쌰

--- main.c
+++ main.c
@@ -12,34 +12,40 @@
 
 int main(){
 
-  printf("Light Torrent\n\n");
+    printf("Light Torrent\n\n");
 
-  int sock = socket(AF_INET,SOCK_STREAM,0);
-  struct sockaddr_in addr;
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-  addr.sin_port = htons(8000);
+    int sock = socket(AF_INET,SOCK_STREAM,0);
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    addr.sin_port = htons(8000);
 
-  int ret = connect(sock,(struct sockaddr*)&addr,sizeof(struct sockaddr));
+    int ret = connect(sock,
+		      (struct sockaddr*)&addr,
+		      sizeof(struct sockaddr));
 
-  char* req = "GET / HTTP 1.1\r\nHost: localhost:8000\r\nAccept: */*\r\nUser-Agent: curl/7.43.0\r\nConnection: close\r\n\r\n";  
-  int sent = send(sock,req,strlen(req),0);
+    char* req =
+	"GET / HTTP 1.1\r\n"
+	"Host: localhost:8000\r\n"
+	"Accept: */*\r\n"
+	"User-Agent: curl/7.43.0\r\n"
+	"Connection: close\r\n\r\n";  
+    int sent = send(sock,req,strlen(req),0);
   
-  int recv_len = 1000;
-  char buf[recv_len];
-  bzero(buf,sizeof(buf));
-  int len = recv(
-		 sock,
-		 buf,
-		 sizeof(buf)-1,
-		 0
-		 );
-  if (len>0){
-    printf("%s\n",buf);
-  }
+    int recv_len = 1000;
+    char buf[recv_len];
+    bzero(buf,sizeof(buf));
+    int len = recv(sock,
+		   buf,
+		   sizeof(buf)-1,
+		   0);
+    if (len>0){
+	printf("%s\n",buf);
+    }
 
-  close(sock);
+    close(sock);
 
-  return 0;
+    return 0;
 }
+ 
  
