bb76530ec69cfe75ae0083e5c17d570f8510a364
changhan <changhanlee@gmail.com>
2016-03-09 11:53:45 -0500

    oh i see

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-//e
-
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-    console.log(request.headers);
-    response.write("hey");
-    console.log(response);
-    response.end();
+    response.end("LAAAAAAH");
 }).listen(8000);
--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-//3
-
 #include <stddef.h>
 #include <stdio.h>
 #include <errno.h>
@@ -14,27 +12,34 @@
 
 int main(){
 
-  printf("Light Torrent\n");
+  printf("Light Torrent\n\n");
 
   int sock = socket(AF_INET,SOCK_STREAM,0);
   struct sockaddr_in addr;
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
   addr.sin_port = htons(8000);
+
   int ret = connect(sock,(struct sockaddr*)&addr,sizeof(struct sockaddr));
-  //printf("%d\n",ret);
-  char* req = "GET / HTTP 1.1\r\nHost: localhost:8000\r\nConnection: keep-alive\r\naccept: */*\r\nuser-agent: changhan\r\n\r\n";
+
+  char* req = "GET / HTTP 1.1\r\nHost: localhost:8000\r\nAccept: */*\r\nUser-Agent: curl/7.43.0\r\nConnection: close\r\n\r\n";  
   int sent = send(sock,req,strlen(req),0);
-  printf("sent: %d\n",sent);
+  
   int recv_len = 1000;
   char buf[recv_len];
-  bzero(buf,strlen(buf));
-  printf("buf: %s\n",buf);
-  int len = recv(sock,buf,recv_len-1,0);
-  printf("received length: %d\n",len);
-  buf[recv_len-1]='\0';
-  printf("received msg:\n%s\n",buf);
+  bzero(buf,sizeof(buf));
+  int len = recv(
+		 sock,
+		 buf,
+		 sizeof(buf)-1,
+		 0
+		 );
+  if (len>0){
+    printf("%s\n",buf);
+  }
+
   close(sock);
 
   return 0;
 }
+ 
