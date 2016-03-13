c78ad509bcb440942753e3b8dc4c297b34c8d4be
changhan <changhanlee@gmail.com>
2016-03-12 20:33:22 -0500

    damn

--- tracker/req.py
+++ tracker/req.py
@@ -2,17 +2,21 @@ import requests
 from bencode import *
 import pprint 
 import codecs
+import binascii
+import unicodedata
+import base64
 
-tracker = "http://glotorrents.com:6969/announce?"+\
-          "info_hash=_%0E%9B%F4%DAX%9F%AE%8F%BE%DEh%2Ah16ff%D0%8E"+\
-          ""
+tracker = "http://glotorrents.com:6969/announce"+\
+          "?info_hash=_%0E%9B%F4%DAX%9F%AE%8F%BE%DEh%2Ah16ff%D0%8E"+\
+          "&peer_id=12345678901234567890"+\
+          "&uploaded=0"+\
+          "&event=started"+\
+          "&compact=1"
 
 r = requests.get(tracker)
 
 res = bdecode(r.text)
 
-pp = pprint.PrettyPrinter(indent=4)
-#pp.pprint(res)
-
 peers = res["peers"]
-print type(peers)
+
+print len(peers)
