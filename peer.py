6c2966194462bbc151e96a4dde259ca64cddd811
changhan <changhanlee@gmail.com>
2016-03-23 16:20:29 -0400

    open chosen port in router

--- peer.py
+++ peer.py
@@ -6,28 +6,16 @@ def connectPeer(peers,hash,id):
     BUFFER_SIZE = 68
     pstr = "BitTorrent protocol"
     hs = pack("!B19s8x20s20s",len(pstr),pstr,hash,id)
+    #hs = chr(19)+pstr+(8*chr(0))+hash+id
     MSG = pack("!2B",1,0)
 
     s = socket(AF_INET, SOCK_DGRAM)
-    #s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    #saddr = '127.0.0.1'
-    #sport = 80
-    #s.bind((saddr, sport))
-    #print s.getsockname()
+    s.bind(("127.0.0.1",9000))
     print (len(peers))
     x = peers[len(peers)-2]
-    TCP_IP = x[0]
-    TCP_PORT = x[1]
-    print TCP_IP,TCP_PORT
-    s.settimeout(4)
-    try:
-        s.connect((TCP_IP,TCP_PORT))
-    except error as socketerror:
-        print("Error: ", socketerror)
-    #s = create_connection((TCP_IP,TCP_PORT))
-    s.send(hs)
-    #s.send(MSG)
-    #data = s.recv(BUFFER_SIZE)
+    s.sendto(hs, x)
+    print "sent"
+    d,a = s.recvfrom(1024)
+    print "recvd"
+    print d,a
     s.close()
-
-    print "recv'd: ", data
